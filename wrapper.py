6bcd6eb1af8c14cdee4dbd706c4a98e04b8012b7
Robert J <robert.jackson@armor.com>
2017-08-28 19:09:59 -0500

    Like Bruce Wayne this commit has no parents.

---
+++ wrapper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Written by:   Robert J.
+#               Robert@scriptmyjob.com
+
+import subprocess
+
+#######################################
+### Main Function #####################
+#######################################
+
+def main():
+    subprocess.call(
+        [
+            "Resources/terraform",
+            "apply",
+            "Terraform"
+        ]
+    )
+
+
+#######################################
+### Execution #########################
+#######################################
+
+if __name__ == "__main__":
+    main()
+
+
+def execute_me_lambda(event, context):
+    out = main()
+    return out
